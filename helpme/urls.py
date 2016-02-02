11676b6d63254bc0fa378859fedecd2d164cee36
tmkn8 <tmkn8@protonmail.ch>
2016-02-02 22:55:57 +0000

    Update OAuth url

--- helpme/urls.py
+++ helpme/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^api/o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^', include('help_requests.urls')),
 ]
