57588429ce9b94f8a7d570ffa58da4b98e68c9b7
Tomasz Knapik <tmkn0808@gmail.com>
2016-03-10 16:14:00 +0000

    Set up app for Heroku

--- helpme/settings.py
+++ helpme/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,8 +26,6 @@ SECRET_KEY = 'gd*)6%26)h3_uwqp2^s4m@b=kujo=sp7om&p4@@c+j(dq$#8vv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -83,13 +83,8 @@ WSGI_APPLICATION = 'helpme.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = dict()
+DATABASES['default'] = dj_database_url.config(default='sqlite:///' + BASE_DIR + '/db.sqlite3')
 
 
 # Password validation
@@ -162,3 +157,7 @@ SITE_ID = 1
 # http://django-allauth.readthedocs.org/en/latest/configuration.html
 
 ACCOUNT_EMAIL_REQUIRED = True
+
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
