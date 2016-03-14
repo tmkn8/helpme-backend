4f5ce6672c8c6f7f8315be15e35aa5bf1c24d58a
Tomasz Knapik <tmkn0808@gmail.com>
2016-03-14 12:00:35 +0000

    Move DefaultMixin to the core package

--- help_requests/views.py
+++ help_requests/views.py
@@ -5,24 +5,6 @@ from .models import HelpRequest
 from .serializers import HelpRequestSerializer
 
 
-class DefaultsMixin(object):
-    """Default settings for view authentication, permissions, filtering and pagination."""
-
-    authentication_classes = (
-        authentication.BasicAuthentication,
-    )
-    permission_classes = (
-        permissions.IsAuthenticated,
-    )
-    paginate_by = 25
-    paginate_by_param = 'page_size'
-    max_paginate_by = 100
-    filter_backends = (
-        filters.SearchFilter,
-        filters.OrderingFilter,
-    )
-
-
 class HelpRequestViewSet(DefaultsMixin, viewsets.ModelViewSet):
     queryset = HelpRequest.objects.all()
     serializer_class = HelpRequestSerializer
