4bc49c7d613380d211963543ed35f515df606870
Tomasz Knapik <tmkn0808@gmail.com>
2016-03-14 12:07:13 +0000

    Add basic UserSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,16 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
 
 class UserSerializer(serializers.Serializer):
+    links = serializers.SerializerMethodField()
+
     class Meta:
-        pass
+        model = get_user_model()
+        fields = ('username', 'date_joined')
+        read_only_fields = ('username', 'date_joined')
+
+    def get_links(self, obj):
+        return {}
