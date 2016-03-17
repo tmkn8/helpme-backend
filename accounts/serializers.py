166c4d2ce93b9e4db1a5aea2770203a2a81ede55
Tomasz Knapik <tmkn0808@gmail.com>
2016-03-17 23:49:50 +0000

    Add logged in user view

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,13 +4,20 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     links = serializers.SerializerMethodField()
 
     class Meta:
         model = get_user_model()
-        fields = ('username', 'date_joined')
-        read_only_fields = ('username', 'date_joined')
+        fields = ['username', 'date_joined', 'is_staff', 'links']
+        read_only_fields = ['username', 'date_joined', 'is_staff']
 
     def get_links(self, obj):
+        request = self.context['request']
         return {}
+
+
+class UserPrivateSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ['email', 'is_staff']
+        read_only_fields = UserSerializer.Meta.read_only_fields + ['is_staff']
--- help_requests/urls.py
+++ help_requests/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from help_requests import views
+from accounts import views as acc_views
+from help_requests import views as hr_views
 
 router = DefaultRouter()
-router.register(r'help-requests', views.HelpRequestViewSet)
+router.register(r'help-requests', hr_views.HelpRequestViewSet)
+router.register(r'users', acc_views.UserViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls))
